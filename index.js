546a83ae8912e913652e6c8d7f5f758df539497d
feyi-tech <jinminetics@gmail.com>
2024-08-18 21:36:43 +0100

    port updated

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/mailer', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
